85590933c91082494c593aaac28d40ed8e67980b
rmdhnz <ramadhanfadhly2@gmail.com>
2023-05-06 21:09:03 +0700

    initiate web form

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbz8W7JtsChWQmZRVWiQdLph20kqWoGAO_oF1Pin31RCTbzK4ADYeiY9_vKbyIwpiDyXQQ/exec";
+const form = document.forms["ieee-form"],
+  btnKirim = document.querySelector(".btn-kirim"),
+  btnLoading = document.querySelector(".btn-loading"),
+  myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      form.reset();
+      myAlert.classList.toggle("d-none");
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
